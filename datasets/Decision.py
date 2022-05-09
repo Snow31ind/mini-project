a1135c41e2e086da6c330aba2305e0b5e1e3ebc7
weneedmoreknowledge <33089415+weneedmoreknowledge@users.noreply.github.com>
2022-05-09 20:27:13 +0700

    Change string to float

---
+++ datasets/Decision.py
@@ -0,0 +1,40 @@
+import numpy as np
+import pandas as pd
+from pandas import read_csv
+import locale
+from locale import atof
+
+locale.setlocale(locale.LC_NUMERIC,'')
+
+#Convert to Real float or Number
+def value_to_float(x):
+    if type(x) == float or type(x) == int:
+        return x
+    if 'K' in x:
+        if len(x) > 1:
+            return float(x.replace('K', '')) * 1000
+        return 1000.0
+    if 'M' in x:
+        if len(x) > 1:
+            return float(x.replace('M', '')) * 1000000
+        return 1000000.0
+    if 'B' in x:
+        return float(x.replace('B', '')) * 1000000000
+    return 0.0
+#
+def percent_to_float(x):
+    if type(x)==float or type(x)==int:
+        return x
+    if '%' in x:
+        if len(x)>1:
+            return float(x.replace('%',''))
+    return 0.0
+#
+
+df = pd.read_csv('VNINDEX_2010-2022.csv',sep=',',thousands=',')
+df['Vol.'] = df['Vol.'].apply(value_to_float)
+df['Change %'] = df['Change %'].apply(percent_to_float)
+
+print(df)
+print(df.dtypes)
+
